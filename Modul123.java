0ab21e91e06c38bda63894ea5ddba02cebbd14f6
Galih Aditya <113104415+mid-aditya@users.noreply.github.com>
2024-08-22 10:13:31 +0700

    Add files via upload

---
+++ Modul123.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugas1;
+
+/**
+ *
+ * @author Adity
+ */
+public class Modul123 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Tugas1 modul=new Tugas1();
+        modul.setVisible(true);
+    }
+    
+}
